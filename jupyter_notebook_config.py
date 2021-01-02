adbe6ae52f6cf99e08271983ee17fd3f9bb94a6c
“liuwch” <liuwch19@gmail.com>
2021-01-02 17:00:37 +0800

    add jupyter

---
+++ jupyter_notebook_config.py
@@ -0,0 +1,8 @@
+# Configuration file for jupyter-notebook.
+
+c.NotebookApp.ip='*'
+c.NotebookApp.password = u'sha1:d6c4a9d846a5:bac5de2f40edb05a81584bf8dac2814aa5ef9309'
+c.NotebookApp.open_browser = False
+c.NotebookApp.port =8900
+c.NotebookApp.allow_remote_access = True
+c.NotebookApp.notebook_dir = '/home/Data1/liuwch'
